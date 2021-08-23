9474613666cdbbe2ed25f251e883b1ad436e062b
Lily Foster <lily@lily.flowers>
2021-08-23 12:13:58 -0400

    update.rb: use Accept instead of Content-Type in Hydra requests

--- update.rb
+++ update.rb
@@ -10,7 +10,7 @@ def fetch_json(url)
   uri = URI(url)
   p url
   Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-    http.request_get(uri.path, {"Content-Type" => "application/json"}) do |response|
+    http.request_get(uri.path, {"Accept" => "application/json"}) do |response|
       response.read_body do |str|
         return JSON.parse(str)
       end
